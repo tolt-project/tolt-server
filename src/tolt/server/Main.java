f9ee15557e380cf1a5a9d97a21f340fc92dc7d9a
suicidalcitygirl <ruby.a.skye@proton.me>
2024-04-27 16:27:14 +1000

    compartmentalize action events and stuff

--- src/tolt/server/Main.java
+++ src/tolt/server/Main.java
@@ -1,29 +1,15 @@
 
 package tolt.server;
 
-import tolt.server.network.Network;
-import tolt.server.service.logging.Logging;
-import tolt.server.service.Config;
-import tolt.server.service.stats.Stats;
 import tolt.server.system.Action;
 
 public class Main {
 
     public static void main (String[] args) {
 
-        Logging.start();
-        Config.load();
-        Stats.load();
-        Logging.log(String.format(
-            "Server has been started %d times.",
-            Stats.increment("server.launch-count")
-        ));
-
-        Network.start();
+        Action.start();
 
         try { System.in.read(); } catch (Exception e) {}
-
-        Network.stop();
 
         Action.shutdown(0, "Process ended.");
     }
--- src/tolt/server/system/Action.java
+++ src/tolt/server/system/Action.java
@@ -1,19 +1,57 @@
 
 package tolt.server.system;
 
+import tolt.server.network.Network;
 import tolt.server.service.logging.Logging;
+import tolt.server.service.Config;
+import tolt.server.service.stats.Stats;
 
 public class Action {
+
+    private static boolean startupCalled = false;
+    public static void start () {
+
+        if (startupCalled) {
+            Logging.err("Startup called twice!"); return;
+        } startupCalled = true;
+
+        Event.onStartup();
+
+        Stats.increment("server.launch-count");
+    }
 
     public static void shutdown (int exitCode) {
 
         shutdown(exitCode, "No reason specified.");
     }
+    private static boolean shutdownCalled = false;
     public static void shutdown (int exitCode, String reason) {
 
-        Logging.log("Shutting down Tolt Server. " + reason);
+        if (shutdownCalled) {
+            Logging.err("Shutdown called twice!"); return;
+        } shutdownCalled = true;
 
+        Logging.log("Shutting down Tolt Server. " + reason);
+        Event.onShutdown();
         Logging.log("Exit: " + String.valueOf(exitCode));
         System.exit(exitCode);
+    }
+
+    private static class Event {
+
+        public static void onStartup () {
+
+            Logging.start();
+            Config.load();
+            Stats.load();
+
+            Network.start();
+        }
+
+        public static void onShutdown () {
+
+            Network.stop();
+            Stats.save();
+        }
     }
 }
